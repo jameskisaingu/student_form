19d60e85cbf1f58aa65321904ec95749071988c2
jameskisaingu <jameskisaingu04@gmail.com>
2026-01-15 01:52:10 +0300

    Update view_students.php

--- view_students.php
+++ view_students.php
@@ -1,3 +1,5 @@
+include 'connect.php';
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,8 +19,28 @@
             <th>Year of study</th>
             <th>Email</th>
         </tr>
-    <a href="index.html">Add New Student</a>
+ 
+        <?php
+        $sql = "SELECT * FROM students";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>
+                        <td>{$row['name']}</td>
+                        <td>{$row['age']}</td>
+                        <td>{$row['course']}</td>
+                        <td>{$row['year']}</td>
+                        <td>{$row['email']}</td>
+                      </tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>No students found.</td></tr>";
+        }
+        ?>
+    </table>
 </div>
 
 </body>
 </html>
+
